b974340df886732ada69efa0e4cb5b6e2e792edb
Vincent FISCHER <17325@ecam.be>
2021-07-05 13:40:45 +0200

    Ajout css et début de remplissage

--- posty/resources/views/layouts/app.blade.php
+++ posty/resources/views/layouts/app.blade.php
@@ -5,8 +5,39 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Posty</title>
+
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
-    <body>
+    <body class="bg-gray-100">
+        <nav class="p-6 bg-white flex justify-between">
+            <ul class="flex items-center">
+                <li>
+                    <a href="" class="p-5">Home</a>
+                </li>
+                <li>
+                    <a href="" class="p-5">Dashboard</a>
+                </li>
+                <li>
+                    <a href="" class="p-5">Posts</a>
+                </li>
+            </ul>
+
+            <ul class="flex items-center">
+                <li>
+                    <a href="" class="p-5">Name</a>
+                </li>
+                <li>
+                    <a href="" class="p-5">Login</a>
+                </li>
+                <li>
+                    <a href="" class="p-5">Register</a>
+                </li>
+                <li>
+                    <a href="" class="p-5">Logout</a>
+                </li>
+            </ul>
+        </nav>
+
         @yield('content')
     </body>
 </html>
