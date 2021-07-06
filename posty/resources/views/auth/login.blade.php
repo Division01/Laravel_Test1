9caa324441baa036f1d1f1ddfb277cb44338e322
Vincent FISCHER <17325@ecam.be>
2021-07-06 11:41:38 +0200

    Creation du Login

---
+++ posty/resources/views/auth/login.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex justify-center">
+        <div class="w-4/12 bg-white p-6 rounded-lg">
+            <form action="{{ route('login') }}" method="POST">
+
+                @csrf 
+
+                <div class="mb-4">
+                    <label for="email" class="sr-only"> Email </label>
+                    <input type="text" name="email" id="email" placeholder="Your email" class="bg-gray-100 border-2 w-full px-2 py-4 rounded-lg" value="{{ old('email') }}">
+                    
+                    @error('email')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+
+                <div class="mb-4">
+                    <label for="password" class="sr-only"> Password </label>
+                    <input type="password" name="password" id="password" placeholder="Your password" class="bg-gray-100 border-2 w-full px-2 py-4 rounded-lg" value="{{ old('password') }}">
+                    
+                    @error('password')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                <div>
+                    <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full"> Login </button>
+                </div>
+
+            </form>
+        </div>
+    </div>
+@endsection
--- posty/routes/web.php
+++ posty/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\RegisterController;
 
 /*
@@ -13,7 +15,10 @@ use App\Http\Controllers\Auth\RegisterController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/dashboard', [RegisterController::class, 'index'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
