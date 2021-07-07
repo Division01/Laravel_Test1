b8c1d1e82a9d0c4e2775ad50accb58a98e271b97
Vincent FISCHER <17325@ecam.be>
2021-07-07 13:23:20 +0200

    Compteur de like ajouté

--- posty/resources/views/users/posts/index.blade.php
+++ posty/resources/views/users/posts/index.blade.php
@@ -6,6 +6,7 @@
             <div class="p-6">
                 <h1 class="text-2xl font-medium mb-1"> {{ $user->name }} </h1>
                 <p> Number of posts : {{ $posts->count() }} </p>
+                <p> Number of received likes : {{ $user->receivedLikes->count() }}</p>
             </div>
             
             <div class="bg-white p-6 rounded-lg">
